6229531bd46b24fe42881d4927fb6ec6d63c7845
ayhanuzumcu <ayhanuzumcu@hotmail.com>
2023-01-09 21:43:03 +0000

    added free drink option to product model and free drink selector box to product template

--- products/models.py
+++ products/models.py
@@ -24,6 +24,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    free_drink = models.BooleanField(default=False, null=True, blank=True)
 
     def __str__(self):
         return self.name
