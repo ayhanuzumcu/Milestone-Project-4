70189c21fa57d58289f98dc807e4fcb7f56820fb
ayhanuzumcu <ayhanuzumcu@hotmail.com>
2023-01-09 18:42:38 +0000

    added bag context and delivery logic

--- premier/settings.py
+++ premier/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -152,5 +153,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 25
+STANDARD_DELIVERY_CHARGE = 2.5
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
