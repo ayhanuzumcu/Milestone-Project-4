2fb2dd55a5deac66fdeab96e271c183c8becde6f
ayhanuzumcu <ayhanuzumcu@hotmail.com>
2023-01-15 23:46:41 +0000

    removed secret key and set debug

--- premier/settings.py
+++ premier/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dhnzk^3z0gr0^k&%%(6w9b9t*n=142rz9cj8uo5$tci(jgm$3b'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['premier-takeaway.herokuapp.com', 'localhost' ]
 
