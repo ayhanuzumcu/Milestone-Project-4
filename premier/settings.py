deda0e1a951e620477cd6fd7dfc11b329be539a9
ayhanuzumcu <ayhanuzumcu@hotmail.com>
2023-01-17 00:07:11 +0000

    initial commit

--- premier/settings.py
+++ premier/settings.py
@@ -20,13 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'ntbn34=17_p)e60ox8%y-9+9wmsk&2s(d8he3$x^cr(zc2+=1k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['premier-takeaway.herokuapp.com', 'localhost' ]
+ALLOWED_HOSTS = ['premier-takeaway.herokuapp.com', 'localhost']
 
 
 # Application definition
