81d633dd8b7a79ac16167def78d36a80d82259f9
ayhanuzumcu <ayhanuzumcu@hotmail.com>
2023-01-18 16:41:28 +0000

    initial commit

--- checkout/migrations/0001_initial.py
+++ checkout/migrations/0001_initial.py
@@ -16,8 +16,12 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Order',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('order_number', models.CharField(editable=False, max_length=32)),
+                ('id', models.BigAutoField(
+                    auto_created=True, primary_key=True,
+                    serialize=False, verbose_name='ID'
+                    )),
+                ('order_number', models.CharField(
+                    editable=False, max_length=32)),
                 ('full_name', models.CharField(max_length=50)),
                 ('email', models.EmailField(max_length=254)),
                 ('phone_number', models.CharField(max_length=20)),
@@ -25,23 +29,44 @@ class Migration(migrations.Migration):
                 ('postcode', models.CharField(max_length=20)),
                 ('town_or_city', models.CharField(max_length=40)),
                 ('street_address1', models.CharField(max_length=80)),
-                ('street_address2', models.CharField(blank=True, max_length=80, null=True)),
+                ('street_address2', models.CharField(
+                    blank=True, max_length=80, null=True
+                    )),
                 ('county', models.CharField(max_length=80)),
                 ('date', models.DateTimeField(auto_now_add=True)),
-                ('delivery_cost', models.DecimalField(decimal_places=2, default=0, max_digits=6)),
-                ('order_total', models.DecimalField(decimal_places=2, default=0, max_digits=10)),
-                ('grand_total', models.DecimalField(decimal_places=2, default=0, max_digits=10)),
+                ('delivery_cost', models.DecimalField(
+                    decimal_places=2, default=0, max_digits=6
+                    )),
+                ('order_total', models.DecimalField(
+                    decimal_places=2, default=0, max_digits=10
+                    )),
+                ('grand_total', models.DecimalField(
+                    decimal_places=2, default=0, max_digits=10
+                    )),
             ],
         ),
         migrations.CreateModel(
             name='OrderLineItem',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('free_drink', models.CharField(blank=True, max_length=20, null=True)),
+                ('id', models.BigAutoField(
+                    auto_created=True, primary_key=True,
+                    serialize=False, verbose_name='ID'
+                    )),
+                ('free_drink', models.CharField(
+                    blank=True, max_length=20, null=True
+                    )),
                 ('quantity', models.IntegerField(default=0)),
-                ('lineitem_total', models.DecimalField(decimal_places=2, editable=False, max_digits=6)),
-                ('order', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='lineitems', to='checkout.order')),
-                ('product', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='products.product')),
+                ('lineitem_total', models.DecimalField(
+                    decimal_places=2, editable=False, max_digits=6
+                    )),
+                ('order', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE,
+                    related_name='lineitems', to='checkout.order'
+                    )),
+                ('product', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE,
+                    to='products.product'
+                    )),
             ],
         ),
     ]
--- checkout/views.py
+++ checkout/views.py
@@ -86,7 +86,9 @@ def checkout(request):
 
         # Save the info to the user's profile if all is well
             request.session['save_info'] = 'save-info' in request.POST
-            return redirect(reverse('checkout_success', args=[order.order_number]))
+            return redirect(reverse(
+                'checkout_success', args=[order.order_number]
+                ))
         else:
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
@@ -105,7 +107,8 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        # Attempt to prefill the form with any info the user maintains in their profile
+        # Attempt to prefill the form with any
+        # info the user maintains in their profile
         if request.user.is_authenticated:
             try:
                 profile = UserProfile.objects.get(user=request.user)
