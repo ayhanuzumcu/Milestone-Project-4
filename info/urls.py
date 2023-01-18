7a9be1f5dac0badb37009c1963d1ae17f0bdb7fd
ayhanuzumcu <ayhanuzumcu@hotmail.com>
2023-01-18 20:13:34 +0000

    added contact info and footer

--- info/urls.py
+++ info/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.info, name='info'),
+    path('delivery_info/', views.delivery_info, name='delivery_info'),
+    path('contact_info/', views.contact_info, name='contact_info'),
 ]
--- info/views.py
+++ info/views.py
@@ -4,6 +4,18 @@ from django.shortcuts import render
 
 
 def info(request):
-    """ A view to return the index page """
+    """ A view to return the info page """
 
     return render(request, 'info/info.html')
+
+
+def delivery_info(request):
+    """ A view to return the delivery information page """
+
+    return render(request, 'info/delivery_info.html')
+
+
+def contact_info(request):
+    """ A view to return the delivery information page """
+
+    return render(request, 'info/contact_info.html')
